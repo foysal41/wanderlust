92e1dbd0660aa89a31a974e48c38efc525eb6d5b
foysal41 <mdfoysal.mf33@example.com>
2026-05-13 22:10:23 +0600

    implement betterAuth initialization, setup sign and login page. Implemented dynamically show navbar also added google auth for login and logout

--- src/app/components/global/Navbar.jsx
+++ src/app/components/global/Navbar.jsx
@@ -1,9 +1,27 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import logo from "@/app/assets/Wanderlast.png"
+import { authClient } from '@/app/lib/auth-client'
+import { Avatar } from "@heroui/react";
 
 export default function Navbar() {
+
+   const { 
+        data: session, 
+        isPending, //loading state
+        error, //error object
+        refetch //refetch the session
+    } = authClient.useSession() 
+
+    const user = session?.user
+
+
+    const handleSignOut = async()=> {
+      await authClient.signOut();
+    }
+
   return (
     <div className='flex justify-between items-center gap-4 p-5 bg-white'>
         <ul className='flex justify-between items-center gap-2'>
@@ -18,10 +36,22 @@ export default function Navbar() {
         </div>
 
          <ul className='flex justify-between items-center gap-2'>
-            <li><Link href={"/profile"}>Profile</Link></li>
-            <li><Link href={"/login"}>Login</Link></li>
+           {
+    
+            user ? <div className='flex items-center gap-2'>
+               <li><Link href={"/profile"}>Profile</Link></li>
+              <li>  <Avatar><Avatar.Image referrerPolicy='no-referrer' alt='jhon' src={user?.image}></Avatar.Image></Avatar></li>        
+              <li><button onClick={handleSignOut} variant="danger" className='text-red-600 cursor-pointer'>Logout</button></li>
+            </div> : <div className='flex items-center gap-2'>
+             <li><Link href={"/login"}>Login</Link></li>
              <li><Link href={"/signup"}>Sign Up </Link></li>
+            </div>
+           }
         </ul>
     </div>
   )
 }
+
+
+     
+          
---
+++ src/app/login/page.jsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { Check } from "@gravity-ui/icons";
+import { authClient } from "@/app/lib/auth-client";
+import { Button, Description, FieldError, Form, Input, Label, Separator, TextField } from "@heroui/react";
+import { Card } from "@heroui/react";
+import { redirect } from "next/navigation";
+
+
+export default function LoginPage() {
+
+    const onSubmit = async(e) => {
+        e.preventDefault()
+
+
+        const  formData = new FormData(e.currentTarget);
+        const user = Object.fromEntries(formData.entries());
+        
+        const { data, error } = await authClient.signIn.email
+        ({
+                email:user.email,
+                password:user.password,
+               
+
+            } )
+
+           if(data){
+            redirect('/')
+           }
+           if(error){
+            alert("error ")
+           }
+
+       
+    }
+
+
+        const handleGoogleSignIn = async()=> {
+        await authClient.signIn.social({
+            provider:'google'
+        })
+    }
+
+  return (
+    <div className="max-w-7xl mx-auto my-6">
+        <div className="text-center">
+            <h1 className="text-3xl font-bold">Login Account</h1>
+            <p>Start Your adventure with wanderlust</p>
+        </div>
+        <Card className="border rounded-none">
+            <Form className="flex w-96 flex-col gap-4" onSubmit={onSubmit} >
+
+
+        <TextField
+        isRequired
+        name="email"
+        type="email"
+        validate={(value) => {
+          if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+            return "Please enter a valid email address";
+          }
+          return null;
+        }}
+      >
+        <Label>Email</Label>
+        <Input placeholder="john@example.com" />
+        <FieldError />
+      </TextField>
+
+
+ 
+      
+      <TextField
+        isRequired
+        minLength={8}
+        name="password"
+        type="password"
+        validate={(value) => {
+          if (value.length < 8) {
+            return "Password must be at least 8 characters";
+          }
+          if (!/[A-Z]/.test(value)) {
+            return "Password must contain at least one uppercase letter";
+          }
+          if (!/[0-9]/.test(value)) {
+            return "Password must contain at least one number";
+          }
+          return null;
+        }}
+      >
+        <Label>Password</Label>
+        <Input placeholder="Enter your password" />
+        <Description>Must be at least 8 characters with 1 uppercase and 1 number</Description>
+        <FieldError />
+      </TextField>
+
+
+      <div className="flex gap-2 justify-center">
+        <Button className={'rounded-none w-full bg-cyan-500'} type="submit">
+          <Check />
+         Login
+        </Button>
+
+      </div>
+    </Form>
+      <div className="flex items-center gap-3 my-4">
+        <Separator className="flex-1" />
+        
+        <div className="whitespace-nowrap">
+          Or Sign up with Google
+        </div>
+        
+        <Separator className="flex-1" />
+      </div>
+          <div>
+              <Button onClick={handleGoogleSignIn} className={'w-full rounded-none'}>Sign in</Button>
+          </div>
+    
+        </Card>
+    </div>
+  )
+}
